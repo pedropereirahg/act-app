4d40381e7fd1adf375aedd1f7f17908966293b32
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2022-05-22 16:11:00 -0300

    chore: improvements on create activity page - select type of activity

--- src/components/TypeOfActivity/index.js
+++ src/components/TypeOfActivity/index.js
@@ -1,43 +1,45 @@
 import * as React from 'react';
-import { Row, Col, Card, Typography, Button } from 'antd';
 import Link from 'next/link';
-import { CheckCircleOutlined } from '@ant-design/icons';
-import styles from './TypeOfActivity.module.scss';
+import { Row, Col, Card, Result, Typography } from 'antd';
+import { EditTwoTone, CheckCircleTwoTone } from '@ant-design/icons';
+import { cyan } from '@ant-design/colors';
 
 function TypeOfActivity() {
   return (
-    <div className={styles.cardWrapper}>
-      <Typography.Title strong level={3}>Selecione a opção correspondente a nova atividade:</Typography.Title>
-      <Row gutter={16} justify="center" align="middle" style={{ height: '100%', width: '100%' }}>
+    <React.Fragment>
+      <Row gutter={16} justify="center" align="middle">
         <Col span={8}>
           <Link href={'/essay-question'}>
-            <Card bordered={false} className={styles.card}>
-              Dissertativa <CheckCircleOutlined />
+            <Card bordered={false} hoverable={true}>
+              <Result
+                icon={<EditTwoTone twoToneColor={cyan.primary} />}
+                title="Discursiva"
+                subTitle={
+                  <Typography.Paragraph type="secondary" ellipsis={{ rows: 2 }}>
+                    Define o enunciado para o aluno dissertar, argumentar ou descrever a resposta
+                  </Typography.Paragraph>
+                }
+              />
             </Card>
           </Link>
         </Col>
         <Col span={8}>
           <Link href={'/'}>
-            <Card bordered={false} className={styles.card}>
-              Múltipla Escolha <CheckCircleOutlined />
-            </Card>
-          </Link>
-        </Col>
-        <Col span={8}>
-          <Link href={'/'}>
-            <Card bordered={false} className={styles.card}>
-              Escolha Única <CheckCircleOutlined />
+            <Card bordered={false} hoverable={true}>
+              <Result
+                icon={<CheckCircleTwoTone twoToneColor={cyan.primary} />}
+                title="Objetiva"
+                subTitle={
+                  <Typography.Paragraph type="secondary" ellipsis={{ rows: 2 }}>
+                    Permite criar alternativas para que o aluno selecione uma ou várias respostas
+                  </Typography.Paragraph>
+                }
+              />
             </Card>
           </Link>
         </Col>
       </Row>
-      <Row align="bottom" style={{ height: '800px', width: '100%' }}>
-        <Col span={24} align='end'>
-          <Button size={'large'} href={'/'} danger>Cancelar</Button>
-        </Col>
-      </Row>
-
-    </div>
+    </React.Fragment>
   )
 };
 
