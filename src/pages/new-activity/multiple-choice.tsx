bfc2b9b409dc78cfdbec5cc67e7d92c5b5e664d3
NayaraFugii <nayarac.neto@gmail.com>
2022-05-25 22:02:31 -0300

    inclusão de textarea para respostas e logica para escolha unica ou escolha multipla

--- src/pages/new-activity/multiple-choice.tsx
+++ src/pages/new-activity/multiple-choice.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useState } from 'react';
 import Router from 'next/router';
 import dynamic from 'next/dynamic';
-import { Row, Col, PageHeader, Typography, Button, Input, Result, Switch, Space, Collapse, Card, Divider } from 'antd';
+import { Row, Col, PageHeader, Typography, Button, Input, Checkbox, Result, Switch, Space, Collapse, Card, Divider, Radio } from 'antd';
 import { CheckCircleTwoTone } from '@ant-design/icons';
 import { cyan, grey, presetPrimaryColors, presetDarkPalettes, } from '@ant-design/colors';
 
@@ -10,19 +10,59 @@ import Metadata from '../../components/Metadata/index';
 const Editor = dynamic(() => import('../../components/Editor'), {
   ssr: false
 });
-
 export default function NewEssayPage() {
+  const { TextArea } = Input;
+  const [isMultipleChoice, setIsMultipleChoice] = useState(Boolean)
+  const [option, setOption] = useState({ "statement": "", isCorrect: false } as any);
+  const [value, setValue] = useState({
+    "statement": "",
+    "options": [],
+    "type": "singleChoice"
+  } as any);
+  const [enunciateValue, setEnunciateValue] = useState({
+    "statement": ""
+  });
+
   const [statement, setStatement] = useState('');
   const editorInitialValue: string = ''
 
   const saveAPI = (content: string) => {
-    setStatement(content);
+    setEnunciateValue({ ...value, statement: content })
   }
+
 
   const [editorIsDirty, onEditorSave, editorConfig] = useEditor(
     editorInitialValue,
     saveAPI
   )
+
+  const editEnunciateValue = (statement: any) => {
+    setEnunciateValue({ ...value, statement: statement })
+
+  }
+
+  const handleChange = (event: any) => {
+    setOption({ ...option, statement: event.target.value })
+  };
+
+  const handleClick = () => {
+    setValue({
+      ...value,
+      options: [...value.options, option]
+    })
+    setOption({ "statement": "", isCorrect: false })
+  };
+
+  const deleteOption = (statement: any) => {
+    const newOptions = value.options.filter((option: any) => option.statement !== statement)
+    setValue({ ...value, options: newOptions })
+  };
+
+  const onChange = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
+    setIsMultipleChoice(checked)
+
+  };
 
   return (
     <Fragment>
@@ -49,20 +89,24 @@ export default function NewEssayPage() {
           /> */}
 
           <Space align="center">
-            <Switch />
+            <Switch defaultChecked onChange={onChange} />
             <Typography.Text>Atividade permite mais de uma alternativa como correta</Typography.Text>
           </Space>
           <Divider orientation="left" orientationMargin="0">
             <Typography.Title level={5} style={{ marginLeft: 5 }}>Enunciado</Typography.Title>
           </Divider>
 
-          {statement === '' ? (
-            <Editor
-              {...editorConfig}
-              initialValue={editorInitialValue}
-            />
+          {enunciateValue.statement === '' ? (
+            <>
+              <Editor
+                {...editorConfig}
+                initialValue={editorInitialValue}
+              />
+
+            </>
+
           ) : (
-            <div dangerouslySetInnerHTML={{ __html: statement }} />
+            <div dangerouslySetInnerHTML={{ __html: enunciateValue.statement }} />
           )}
 
           <Divider />
@@ -82,18 +126,50 @@ export default function NewEssayPage() {
             </Collapse.Panel>
           </Collapse>
 
-
-
           <br />
-          {statement === '' ? (
+          {enunciateValue.statement === '' ? (
             <Button onClick={onEditorSave} size="large" type="primary" disabled={!editorIsDirty}>
               Salvar
             </Button>
           ) : (
-            <Button onClick={() => setStatement('')} size="large" type="primary">
-              Editar
-            </Button>
+            <>
+              <Button onClick={() => setStatement('')} size="large" type="primary">
+                Editar
+              </Button>
+              {isMultipleChoice === false ?
+                <Row>
+                  <Radio.Group>
+                    {value.options.map((option: any, i: any) => (
+                      <Row key={i} justify="center" align="middle" style={{ height: '50px', width: '100%' }}>
+                        <Col span={24}><Radio value={i}>{option.statement} <Button danger onClick={() => deleteOption(option.statement)}>x</Button></Radio></Col>
+                      </Row>
+                    ))}
+
+                  </Radio.Group>
+                </Row> :
+                <div>
+                  {value.options.map((option: any, i: any) => (
+                    <Row key={i} justify="center" align="middle" style={{ height: '50px', width: '100%' }}>
+                      <Col span={24}><Checkbox key={i}>{option.statement} <Button danger onClick={() => deleteOption(option.statement)}>x</Button></Checkbox></Col>
+                    </Row>
+                  ))
+                  }
+                </div>
+              }
+              <TextArea
+                onChange={handleChange}
+                placeholder="Digite a questão"
+                autoSize={{ minRows: 2, maxRows: 3 }}
+                value={option.statement}
+              />
+              <Button size={'large'} type="primary" onClick={handleClick}>Adicionar</Button>
+            </>
           )}
+          <Row>
+            <Button onClick={editEnunciateValue} size="large" type="primary">
+              Salvar Questão
+            </Button>
+          </Row>
 
         </Col>
       </Row>
