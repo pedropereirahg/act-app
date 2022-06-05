cc0b9d39646d3c870ae9dbd5ad476f91fadfe79a
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2022-06-04 23:59:31 -0300

    chore: improvements to essay

--- src/commons/environment.ts
+++ src/commons/environment.ts
@@ -1,4 +1,7 @@
 export default {
-  CACHE_CONTROL: process.env.CACHE_CONTROL || '',
-  ACTIVITIES_API_URL: process.env.ACTIVITIES_API_URL,
-} as Record<string, any>
+  CACHE_CONTROL: process.env.NEXT_PUBLIC_CACHE_CONTROL || '',
+  ACTIVITIES_API_URL: process.env.NEXT_PUBLIC_ACTIVITIES_API_URL,
+} as {
+  CACHE_CONTROL: string;
+  ACTIVITIES_API_URL: string;
+}
