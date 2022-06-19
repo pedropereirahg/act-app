27df2bf5141f51ad981833ad6de9e7e4af0d2f69
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2022-06-19 17:12:45 -0300

    chore: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,58 @@
+import { Fragment, useEffect } from 'react'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 import 'antd/dist/antd.css'
-import type { AppProps } from 'next/app'
 
 import './globals.css'
+import env from '../commons/environment'
+import * as gtag from '../commons/lib/gtag'
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  gtag.event({
+    action: metric.name,
+    category: metric.label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    label: metric.id,
+    value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
+    non_interaction: true,
+  })
+}
 
 export default function ActivityApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <Fragment>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${env.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${env.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
